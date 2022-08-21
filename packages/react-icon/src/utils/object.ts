85e268a0726bdf969b557c66aeb2408188ef35eb
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-08-21 18:36:15 -0400

    Setup Package

---
+++ packages/react-icon/src/utils/object.ts
@@ -0,0 +1,8 @@
+// https://fettblog.eu/typescript-hasownproperty/
+export function hasOwnProperty<X extends object, Y extends PropertyKey>(
+    obj: X,
+    prop: Y,
+): obj is X & Record<Y, unknown> {
+    // eslint-disable-next-line no-prototype-builtins
+    return obj.hasOwnProperty(prop);
+}
