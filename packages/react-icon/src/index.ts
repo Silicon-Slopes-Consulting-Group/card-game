ca94a942cb50e5e0a2d05b613177d211fae0c3f2
Charles-François St-Cyr <charlesf2001@gmail.com>
2022-08-21 21:31:53 -0400

    clean

--- packages/react-icon/src/index.ts
+++
@@ -1,7 +0,0 @@
-import Icon from './Icon/Icon';
-import IconRender from './IconFactory/IconRender';
-import IconStack from './IconStack/IconStack';
-
-export default Icon;
-export { IconStack, IconRender };
-export type { IconType } from './Icon.types';
